6863743792669042176e097c1ea72e9d1d22661c
Daven <davensteen@gmail.com>
2022-10-19 01:11:00 -0500

    finsih employee test

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -6,3 +6,22 @@ test('creates an Employee object', () => {
     expect(employee.id).toBe('22689');
     expect(employee.email).toBe('me@gmail.com')
 })
+
+test('test getName()', () => {
+    const mockName = 'Daven';
+    const employee = new Employee(mockName);
+    expect(employee.getName()).toBe(mockName);
+
+})
+
+test('test getId()', () => {
+    const mockID = '22689';
+    const employee = new Employee('dave', mockID);
+    expect(employee.getID()).toBe(mockID);
+})
+
+test('test getEmail()', () => {
+    const mockEmail = 'me@gmail.com';
+    const employee = new Employee('dave', '22689', mockEmail);
+    expect(employee.getEmail()).toBe(mockEmail);
+})
